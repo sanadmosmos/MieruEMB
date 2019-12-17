f8562e1dd1e39ea479c94801b976d515a13a598f
sanadmosmos <sanadryujiwa@gmail.com>
2019-12-17 17:56:02 +0900

    delete connection program

--- define.h
+++ define.h
@@ -41,20 +41,6 @@ typedef struct {
 	int drop_speed;
 } flag;
 
-typedef union {
-	unsigned char byte;
-	struct {
-		unsigned char bit0:1;
-		unsigned char bit1:1;
-		unsigned char bit2:1;
-		unsigned char bit3:1;
-		unsigned char bit4:1;
-		unsigned char bit5:1;
-		unsigned char bit6:1;
-		unsigned char bit7:1;
-	};
-} switch_data;
-
 typedef enum {
 	INIT,
 	NEW_BLOCK,
